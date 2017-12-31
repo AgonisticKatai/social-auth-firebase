eab6e5a6c820f934e02e805e3faea0f2b2e967db
Nacho Torrella <nachotorrella@gmail.com>
2017-12-31 09:05:14 +0100

    change components class

--- src/components/LoginButtons.js
+++ src/components/LoginButtons.js
@@ -1,28 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 
-class LoginButtons extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <button
-          onClick={this.props.googleAuth}
-          className="btn btn-social btn-google"
-        >
-          <span className="fa fa-google" />Login with Google
-        </button>
-        <button
-          onClick={this.props.facebookAuth}
-          className="btn btn-social btn-facebook"
-        >
-          <span className="fa fa-facebook" />Login with Facebook
-        </button>
-      </div>
-    );
-  }
+const LoginButtons = props => {
+  return (
+    <div>
+      <button
+        onClick={props.googleAuth}
+        className="btn btn-social btn-google"
+      >
+        <span className="fa fa-google" />Login with Google
+      </button>
+      <button
+        onClick={props.facebookAuth}
+        className="btn btn-social btn-facebook"
+      >
+        <span className="fa fa-facebook" />Login with Facebook
+      </button>
+    </div>
+  );
 }
 
 export default LoginButtons;
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
-class UserCard extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="card">
-        <img
-          className="card-img-top rounded-circle"
-          src={this.props.userData.user.photoURL}
-          alt={this.props.userData.user.displayName}
-        />
-        <div className="card-body">
-          <h5 className="card-title">Logged as</h5>
-          <p className="card-text">{this.props.userData.user.displayName}</p>
-          <p>
-            <small className="card-text">
-              {this.props.userData.user.email}
-            </small>
-          </p>
-          <button onClick={this.props.logOut} className="btn btn-danger">
-            Logout
-          </button>
-        </div>
+const UserCard = props => {
+  return (
+    <div className="card">
+      <img
+        className="card-img-top rounded-circle"
+        src={props.userData.user.photoURL}
+        alt={props.userData.user.displayName}
+      />
+      <div className="card-body">
+        <h5 className="card-title">Logged as</h5>
+        <p className="card-text">{props.userData.user.displayName}</p>
+        <p>
+          <small className="card-text">
+            {props.userData.user.email}
+          </small>
+        </p>
+        <button onClick={props.logOut} className="btn btn-danger">
+          Logout
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default UserCard;
